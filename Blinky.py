4ed1f7a0889133e93174134bf5709aa44d6a6303
Kaleb Ames <44175737+Deatification@users.noreply.github.com>
2018-10-15 19:32:46 -0700

    Update and rename Blink.py to Blinky.py

--- Blinky.py
+++ Blinky.py
@@ -1,9 +1,11 @@
-#Blink.py
+#Makes an LED circuit hooked to the Pi blike really fast
 import time
 import RPi.GPIO as GPIO
 while True:
+#Makes an infinite loop
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(4, GPIO.OUT)
+#Pin number 7 but GPIO pin number 4
     GPIO.output(4, True)
     time.sleep(.05)
     GPIO.output(4, False)
